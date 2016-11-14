3fda53ebedc84fe9dfeab9f4b0f68c3870af8f8c
vmaccatrozzo <valentina.maccatrozzo@gmail.com>
2016-11-14 16:33:51 +0100

    more ontologies

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,11 @@
 const properties2skip =['http://schema.org/sameAs','http://www.w3.org/2000/01/rdf-schema#comment','http://purl.org/dc/terms/identifier']
-const ontologies = ['http://downloads.dbpedia.org/2016-04/dbpedia_2016-04.owl','http://dublincore.org/2012/06/14/dcterms.ttl','http://xmlns.com/foaf/spec/index.rdf','http://www.w3.org/2000/01/rdf-schema.rdf','http://www.w3.org/1999/02/22-rdf-syntax-ns.rdf','http://www.w3.org/2002/07/owl.rdf','https://www.w3.org/2009/08/skos-reference/skos.rdf']
+const ontologies = ['http://downloads.dbpedia.org/2016-04/dbpedia_2016-04.owl','http://dublincore.org/2012/06/14/dcterms.ttl','http://xmlns.com/foaf/spec/index.rdf','http://www.w3.org/2000/01/rdf-schema.rdf','http://www.w3.org/1999/02/22-rdf-syntax-ns.rdf','https://www.w3.org/2009/08/skos-reference/skos.rdf','https://www.w3.org/ns/prov-o.owl','http://www.w3.org/2002/07/owl.ttl']
 			
 const $rdf = require('rdflib');
 const store = $rdf.graph()
 const timeout = 5000 // 5000 ms timeout
 const fetcher = new $rdf.Fetcher(store, timeout)
+
 
 function pushTriple(triples_array, object, provenance,property_uri,subject_uri,propertiesLabels){
 		
@@ -34,6 +35,7 @@ function getPropertyLabel(property,propertiesLabels){
 			var label = parts[parts.length-1]
 		}else{
 			var label = propertiesLabels[property]	
+			// console.log(label)
 		}
 	}
 	return label
@@ -67,6 +69,21 @@ function uniqueArrayObjects(array2sort){
 	}
 	return result
 }
+
+function test_fetcher(){
+	var url ='https://www.wikidata.org/wiki/Q1067'
+	fetcher.nowOrWhenFetched(url, function(ok, body, xhr) {
+		if (!ok) {
+			console.log("Oops, something happened and couldn't fetch data");
+		} else {
+			var data = store.statementsMatching($rdf.sym(url),undefined,undefined)
+			for(var t=0;t<data.length;t++){
+				console.log(data[t].subject.value)
+				console.log(data[t].object.value)
+			}
+		}
+	})
+}
 function getData(subject, property, objects, prov, primary_data,propertyLabels){	
 	if (properties2skip.indexOf(property) ==-1 & property.indexOf('http://www.iana.org')!=0){
 		var unique_objects = uniqueArrayObjects(objects)
@@ -91,83 +108,114 @@ export default function (navigateTo, dispatch) {
 			};
 			
 			var primary_data = new Array();
-			var propertiesLabels = new Map()
-			var labels_data = []
-			for(var i=0; i<ontologies.length; i++){
-				var ontologyToLoad = ontologies[i]
-				fetcher.nowOrWhenFetched(ontologyToLoad, function(ok, body, xhr) {
-					if (!ok) {
-						console.log("Oops, something happened and couldn't fetch data");
-					} else {
-						// console.log("Loaded " + ontologyToLoad)
-						labels_data.push(store.statementsMatching(undefined,$rdf.sym('http://www.w3.org/2000/01/rdf-schema#label'),undefined))
-					}
-					
-					for(var i = 0; i<labels_data.length; i++){
-						for(var y = 0; y<labels_data[i].length; y++){
-							if(labels_data[i][y].object.lang){
-								if(labels_data[i][y].object.lang == 'en'){
-									var sub = labels_data[i][y].subject.value
-									var obj = labels_data[i][y].object.value
-									propertiesLabels[sub] = obj
-								}
-							}else{
-								var sub = labels_data[i][y].subject.value
-								var obj = labels_data[i][y].object.value
-								propertiesLabels[sub] = obj
-							}
-							
-						}
-					}	
-					fetcher.nowOrWhenFetched(url_complete, function(ok, body, xhr) {
+			var propertiesLabels = new Map();
+			// test_fetcher()
+			fetcher.nowOrWhenFetched(ontologies[0], function(ok, body, xhr) {
+				if (!ok) {
+					console.log("Oops, something happened and couldn't fetch data");
+				} else {
+					fetcher.nowOrWhenFetched(ontologies[1], function(ok, body, xhr) {
 						if (!ok) {
 							console.log("Oops, something happened and couldn't fetch data");
 						} else {
-							var s = $rdf.sym(url),
-							prop = store.each(s, undefined);
-					
-							var sameAs_prop = $rdf.sym("http://schema.org/sameAs");
-							var sameASes = store.each(s,sameAs_prop,undefined);
-							for (var i=0; i<sameASes.length; i++){
-								var temp = sameASes[i].value;
-								if (temp.indexOf('http://dbpedia.org/resource/')==0){
-									var Dbpedia_sameAs = temp;
-								}
-							}
-
-							var unique_prop = uniqueArray(prop)
-							
-							for(var i=0; i<unique_prop.length; i++){       	
-								var prop = unique_prop[i];
-								var objects = store.each(s,$rdf.sym(prop),undefined);
-
-								primary_data = getData(url,prop,objects,'viaf',primary_data,propertiesLabels)
-								
-							}
-							fetcher.nowOrWhenFetched(Dbpedia_sameAs, function(ok, body, xhr) {
+							fetcher.nowOrWhenFetched(ontologies[2], function(ok, body, xhr) {
 								if (!ok) {
 									console.log("Oops, something happened and couldn't fetch data");
 								} else {
-									var s = $rdf.sym(Dbpedia_sameAs)
-									var prop = store.each(s, undefined)
-									var unique_prop = uniqueArray(prop)
-									
-									for(var i=0; i<unique_prop.length; i++){       	
-										var prop = unique_prop[i];
-						
-										var propertyLabel = getPropertyLabel(prop,propertiesLabels)
-										var objects = store.each(s,$rdf.sym(prop),undefined);
-										primary_data = getData(Dbpedia_sameAs,prop,objects,'dbpedia',primary_data,propertiesLabels)	
-									}
-									dispatch({type: "RECEIVE_URL", 
-											data: primary_data
+									fetcher.nowOrWhenFetched(ontologies[3], function(ok, body, xhr) {
+										if (!ok) {
+											console.log("Oops, something happened and couldn't fetch data");
+										} else {
+											fetcher.nowOrWhenFetched(ontologies[4], function(ok, body, xhr) {
+												if (!ok) {
+													console.log("Oops, something happened and couldn't fetch data");
+												} else {
+													fetcher.nowOrWhenFetched(ontologies[5], function(ok, body, xhr) {
+														if (!ok) {
+															console.log("Oops, something happened and couldn't fetch data");
+														} else {
+															fetcher.nowOrWhenFetched(ontologies[6], function(ok, body, xhr) {
+																if (!ok) {
+																	console.log("Oops, something happened and couldn't fetch data");
+																} else {
+																	fetcher.nowOrWhenFetched(ontologies[7], function(ok, body, xhr) {
+																		if (!ok) {
+																			console.log("Oops, something happened and couldn't fetch data");
+																		} else {
+																			fetcher.nowOrWhenFetched(url_complete, function(ok, body, xhr) {
+																				if (!ok) {
+																					console.log("Oops, something happened and couldn't fetch data");
+																				} else {
+																					var labels_data = store.statementsMatching(undefined,$rdf.sym('http://www.w3.org/2000/01/rdf-schema#label'),undefined)																
+																					for(var i = 0; i<labels_data.length; i++){
+																						if(labels_data[i].object.lang){
+																							if(labels_data[i].object.lang == 'en'){
+																								var sub = labels_data[i].subject.value
+																								var obj = labels_data[i].object.value
+																								propertiesLabels[sub] = obj
+																							}
+																						}else{
+																							var sub = labels_data[i].subject.value
+																							var obj = labels_data[i].object.value
+																							propertiesLabels[sub] = obj
+																						}
+																					}	
+																					var s = $rdf.sym(url),
+																					prop = store.each(s, undefined);																			
+																					var sameAs_prop = $rdf.sym("http://schema.org/sameAs");
+																					var sameASes = store.each(s,sameAs_prop,undefined);
+																					for (var i=0; i<sameASes.length; i++){
+																						var temp = sameASes[i].value;
+																						if (temp.indexOf('http://dbpedia.org/resource/')==0){
+																							var Dbpedia_sameAs = temp;
+																						}
+																					}
+
+																					var unique_prop = uniqueArray(prop)
+																					
+																					for(var i=0; i<unique_prop.length; i++){       	
+																						var prop = unique_prop[i];
+																						var objects = store.each(s,$rdf.sym(prop),undefined);
+																						primary_data = getData(url,prop,objects,'Viaf',primary_data,propertiesLabels)
+																						
+																					}
+																					fetcher.nowOrWhenFetched(Dbpedia_sameAs, function(ok, body, xhr) {
+																						if (!ok) {
+																							console.log("Oops, something happened and couldn't fetch data");
+																						} else {
+																							var s = $rdf.sym(Dbpedia_sameAs)
+																							var unique_prop = store.each(s, undefined)
+																							
+																							for(var i=0; i<unique_prop.length; i++){     
+
+																								var prop = unique_prop[i].value;
+																								console.log(prop)
+																								var objects = store.each(s,$rdf.sym(prop),undefined);
+																								primary_data = getData(Dbpedia_sameAs,prop,objects,'DBpedia',primary_data,propertiesLabels)	
+																							}
+																							dispatch({type: "RECEIVE_URL", 
+																									data: primary_data
+																							})
+																						}
+																					})
+																				}
+																			})
+																		}
+																	})
+																}
+															})
+														}
+													})
+												}
+											})
+										}
 									})
 								}
 							})
 						}
 					})
-				})
-			}
+				}
+			})
 		},
 		
 		onValueClick: function (property,id) {
